26ac5e5faddc657a860763f1e9ca236df8185b0b
Sam Hollings <52575338+SamHollings@users.noreply.github.com>
2022-12-09 09:07:55 +0000

    Reformatted code to meet pep8 (pylint, black, flake8) fixed some bugs, checked doctest runs. Make stub test files.

--- src/disclosure_control_check/__init__.py
+++ src/disclosure_control_check/__init__.py
@@ -1,32 +1,36 @@
-'''
+"""
 Functions that do:
-[Done]: check pandas Series for: values less than 5
-[Done]: check pandas Series for: values not divisible by 5 ( x % 5 <> 0)
-[ToDo]: get the rows IDs of any "failed" checks
-[ToDo]: apply the above across a dataframe to designated columns returning column and row IDs of failures.
-'''
+DONE: check pandas Series for: values less than 5
+DONE: check pandas Series for: values not divisible by 5 ( x % 5 <> 0)
+TODO: get the rows IDs of any "failed" checks
+TODO: apply the above across a dataframe to designated columns returning
+    column and row IDs of failures.
+"""
 import pandas as pd
-import numpy as np 
+
 
 def check_series_sdc(column: pd.Series) -> pd.Series:
-    '''Checks the series following the disclosure control rules
+    """Checks the series following the disclosure control rules
     Attributes:
-        columns (pd.Series): the column being checked provided as a pandas Series
+        columns (pd.Series): the column being checked provided as a pandas
+                            Series
     Returns:
         pd.Series: a mask which describes which rows passed and which failed
     Example:
         >>> x = pd.Series([0,1,5,10,11,101], name='count')
         >>> check_series_sdc(x).to_list()
-        [1,0,1,1,0,0]
-    '''
-    
+        [False, False, True, True, False, False]
+    """
+
     divisible_by_five = ((column % 5) == 0).fillna(1)
     greater_equal_five = column >= 5
-    
+
     mask = divisible_by_five.combine(greater_equal_five, min, fill_value=0)
-    
+
     return mask
 
 
 if __name__ == "__main__":
     import doctest
+
+    doctest.testmod()
--- src/make_data/__init__.py
+++ src/make_data/__init__.py
@@ -1,4 +1,41 @@
-'''
-Functions that do:
-[ToDo]: make some test data with known disclosure control failures
-'''
+"""This code will make test dataset for use in the other scripts"""
+import pandas as pd
+
+
+def test_data() -> pd.DataFrame:
+    """Returns a pandas dataframe with some common test cases
+    Args:
+        None
+    Returns:
+        pd.Dataframe: the test data
+    Example:
+        >>> x = test_data()
+        >>> len(x)
+        6
+        >>> x.loc[3, 'cat_field']
+        'John Doe'
+    """
+    df_test_data = pd.DataFrame(
+        dict(
+            id=[0, 1, 2, 3, 4, 5],
+            count=[0, 1, 5, 10, 11, 101],
+            cat_field=["a", "b", "a", "John Doe", "a", "b"],
+            freetext_field=[
+                "",
+                "a",
+                """I thought Doctor Smith, who lived at 1 privett drive, was
+                very friendly xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx""",
+                "b",
+                "c",
+                None,
+            ],
+        )
+    )
+
+    return df_test_data
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
