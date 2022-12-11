aa1737c16c2b708f4c26cec358b55de6dc6b481b
Sam Hollings <52575338+SamHollings@users.noreply.github.com>
2022-12-11 18:35:17 +0000

    Added basic tests - specifically doctests to unittest files

--- tests/test_make_data.py
+++ tests/test_make_data.py
@@ -1,12 +1,12 @@
 import unittest
 import doctest
-import src.disclosure_control_check
+import src.make_data
 
 
 def load_tests(loader, tests, ignore):
     """This creates a unittest.TestCase from the doctests described in the
     module"""
-    tests.addTests(doctest.DocTestSuite(src.disclosure_control_check))
+    tests.addTests(doctest.DocTestSuite(src.make_data))
     return tests
 
 
