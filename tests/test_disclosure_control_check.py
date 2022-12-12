dbcd7f284acab50674a928d0fec50a30846eb97e
AdamHollings <adam.hollings1@nhs.net>
2022-12-12 22:57:09 +0000

    Added a unit test for disclosure_control_check

--- tests/test_disclosure_control_check.py
+++ tests/test_disclosure_control_check.py
@@ -1,6 +1,8 @@
 import unittest
 import doctest
-import src.disclosure_control_check
+from src.disclosure_control_check import check_series_sdc
+import src.make_data
+import pandas as pd
 
 
 def load_tests(loader, tests, ignore):
@@ -8,6 +10,13 @@ def load_tests(loader, tests, ignore):
     module"""
     tests.addTests(doctest.DocTestSuite(src.disclosure_control_check))
     return tests
+
+class test_check_series_sdc(unittest.TestCase):
+    def runTest(self):
+        data = pd.Series([10,15,20,25,30])
+        self.assertEqual(check_series_sdc(data), 
+        pd.Series({0 : True, 1: True, 2: True, 3: True, 4: True}), 
+        "incorrect check for sdc")
 
 
 if __name__ == "__main__":
