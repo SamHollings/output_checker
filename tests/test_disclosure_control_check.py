604acf036b72261a169e79572d877a55768a78ce
AdamHollings <adam.hollings1@nhs.net>
2022-12-19 21:11:01 +0000

    addded some tests to the dislocusre control check test

--- tests/test_disclosure_control_check.py
+++ tests/test_disclosure_control_check.py
@@ -1,10 +1,12 @@
 """disclosure_control_check tests"""
+import sys
+
+sys.path.append("../src")
+
 import unittest
 import doctest
 import pandas as pd
 from src.disclosure_control_check import check_series_sdc
-import src.make_data
-
 
 def load_tests(loader, tests, ignore):  # pylint: disable=unused-argument
     """This creates a unittest.TestCase from the doctests described in the
@@ -17,12 +19,18 @@ def load_tests(loader, tests, ignore):  # pylint: disable=unused-argument
 class TestCheckSeriesSdc(unittest.TestCase):
     """Testing the check_series_sdc function"""
 
-    def output_correct(self):
+    def test_output_correct(self):
         """checks the output of the function is as expected"""
-        data = pd.Series([10, 15, 20, 25, 30])
-        result = check_series_sdc(data).tolist()
-        expected = pd.Series([True, True, True, True, True]).tolist()
-        self.assertEqual(result, expected, "incorrect check for sdc")
+        self.assertEqual(check_series_sdc(pd.Series([10, 15, 20, 25, 30])).tolist(), 
+                        pd.Series([True, True, True, True, True]).tolist(), 
+                        "incorrect check for sdc")
+        self.assertEqual(check_series_sdc(pd.Series([5, 31, 1005, -5, -100])).tolist(), 
+                        pd.Series([False, True, True, False, True]).tolist(), 
+                        "incorrect check for sdc")
+        self.assertEqual(check_series_sdc(pd.Series(["Egg", 27, -17, "Adam", 10052022])).tolist(), 
+                        pd.Series([False, False, False, False, False]).tolist(),  # should be false as the value is date like
+                        "incorrect check for sdc")
+                    
 
 
 if __name__ == "__main__":
