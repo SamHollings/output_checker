06b05621fb5bb0abfd8446e662e03c4caf85a026
Sam Hollings <52575338+SamHollings@users.noreply.github.com>
2022-12-21 17:35:08 +0100

    Added tests for the new functions and a new utility to help with testing dataframes

--- tests/test_disclosure_control_check.py
+++ tests/test_disclosure_control_check.py
@@ -2,8 +2,10 @@
 import unittest
 import doctest
 import pandas as pd
-from src.disclosure_control_check import check_series_sdc
+import numpy as np
+import src.disclosure_control_check
 import src.make_data
+import src.utils as utils
 
 
 def load_tests(loader, tests, ignore):  # pylint: disable=unused-argument
@@ -17,12 +19,35 @@ def load_tests(loader, tests, ignore):  # pylint: disable=unused-argument
 class TestCheckSeriesSdc(unittest.TestCase):
     """Testing the check_series_sdc function"""
 
-    def output_correct(self):
+    def test_output_correct(self):
         """checks the output of the function is as expected"""
         data = pd.Series([10, 15, 20, 25, 30])
-        result = check_series_sdc(data).tolist()
+        result = src.disclosure_control_check.check_series_sdc(data).tolist()
         expected = pd.Series([True, True, True, True, True]).tolist()
         self.assertEqual(result, expected, "incorrect check for sdc")
+
+
+class TestReturnSdcDataframeFails(unittest.TestCase):
+    """Testing the return_sdcs_dataframe_fails function"""
+
+    def test_output_correct(self):
+        """checks the output of the function is as expected"""
+        x_dict = {
+            "place": ["York", "Sheffield", "Leeds"],
+            "count": [0, 5, 50],
+            "count2": [100, 20, 105],
+            "count3": [0, 30, 200],
+        }
+        x = pd.DataFrame(x_dict)
+        result = src.disclosure_control_check.return_sdc_dataframe_fails(x)
+        expected = pd.DataFrame(
+            dict(count=[0.0, 5.0], count3=[0.0, np.NaN]), index=[0, 1]
+        )
+        self.assertEqual(
+            utils.prep_df_for_tests(result),
+            utils.prep_df_for_tests(expected),
+            "incorrect identification of error rows",
+        )
 
 
 if __name__ == "__main__":
