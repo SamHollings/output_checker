ca33de8dfd932007db2524d1db4e38dc082b0f6f
twinbanj <twinbanj@gmail.com>
2023-01-06 21:25:49 +0000

    Added To DO to the to do in SRC

--- src/disclosure_control_check/__init__.py
+++ src/disclosure_control_check/__init__.py
@@ -5,9 +5,10 @@ DONE: check pandas Series for: values not divisible by 5 ( x % 5 <> 0)
 TODO: get the rows IDs of any "failed" checks
 TODO: apply the above across a dataframe to designated columns returning
     column and row IDs of failures.
-TODO: entity recognition on a string
+TODO: create entitiy recongition of a string function
 """
 import pandas as pd
+import numpy as np
 
 
 def check_series_sdc(column: pd.Series) -> pd.Series:
@@ -22,12 +23,43 @@ def check_series_sdc(column: pd.Series) -> pd.Series:
         >>> check_series_sdc(x).to_list()
         [False, False, False, True, False, False]
     """
+
     divisible_by_five = ((column % 5) == 0).fillna(1)
-    greater_equal_ten = abs(column) >= 10
+    greater_equal_ten = column >= 10
 
     mask = divisible_by_five.combine(greater_equal_ten, min, fill_value=0)
 
     return mask
+
+
+def return_sdc_dataframe_fails(data: pd.DataFrame) -> pd.DataFrame:
+    """Apply the SDC rules to a dataframe, and then returns only the rows /
+    columns which failed
+    Attributes:
+        data (pd.DataFrame): the data being checked provided as a pandas
+                            DataFrame
+    Returns:
+        pd.DataFrame: a mask which contains the failure entries
+    Example:
+        >>> #ToDo: add an example for return_sdc_dataframe_fails
+        >>> x_dict = {"place":["York", "Sheffield", "Leeds"],
+        ... 'count':[0,5,50],
+        ... 'count2':[100,20,105],
+        ... 'count3':[0,30, 200]
+        ... }
+        >>> x = pd.DataFrame(x_dict)
+        >>> print(return_sdc_dataframe_fails(x))
+           count  count3
+        0    0.0     0.0
+        1    5.0     NaN
+    """
+    data_numeric = data.select_dtypes(include=np.number)
+
+    # apply sdc to COLUMNs
+    data_mask = data_numeric.apply(check_series_sdc, axis=0)
+
+    return data[~data_mask].dropna(axis=1, how="all").dropna(axis=0, how="all")
+
 
 
 if __name__ == "__main__":
