2bbfb713df0318207c042d914c05b703aeab323b
Adam Hollings <adam.hollings1@nhs.net>
2023-07-17 22:28:04 +0100

    Update file_type_check.py
    Linted better

--- src/file_type_check.py
+++ src/file_type_check.py
@@ -1,15 +1,15 @@
 import os
 
 def get_file_type(file_path):
-  """Gets the file type of a file.
+    """Gets the file type of a file.
 
-  Args:
-    file_path: The path to the file.
+    Args:
+      file_path: The path to the file.
 
-  Returns:
-    The file type of the file.
-  """
+    Returns:
+      The file type of the file.
+    """
 
-  file_extension = os.path.splitext(file_path)[1]
-  file_type = file_extension.lstrip(".").lower()
-  return file_type
+    file_extension = os.path.splitext(file_path)[1]
+    file_type = file_extension.lstrip(".").lower()
+    return file_type
