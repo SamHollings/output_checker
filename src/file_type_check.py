6e232982b007544cd53c904376116c03d09981ab
Sam Hollings <52575338+SamHollings@users.noreply.github.com>
2023-07-20 07:55:31 +0000

    Added a basic test and small edit for flake8

--- src/file_type_check.py
+++ src/file_type_check.py
@@ -1,4 +1,6 @@
+"""Functions which check the type of the files"""
 import os
+
 
 def get_file_type(file_path):
     """Gets the file type of a file.
@@ -8,8 +10,19 @@ def get_file_type(file_path):
 
     Returns:
       The file type of the file.
+
+    Example:
+      >>> get_file_type("README.md")
+      'md'
     """
 
     file_extension = os.path.splitext(file_path)[1]
     file_type = file_extension.lstrip(".").lower()
     return file_type
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
+  
